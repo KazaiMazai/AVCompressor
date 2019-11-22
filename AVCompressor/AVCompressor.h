209a2f121969013c522ab4cebc8578e1c9c5c128
Sergey Kazakov <kazaimazai@gmail.com>
2019-11-22 17:24:18 +0300

    Initial Commit

---
+++ AVCompressor/AVCompressor.h
@@ -0,0 +1,19 @@
+//
+//  AVCompressor.h
+//  AVCompressor
+//
+//  Created by Sergey Kazakov on 22/11/2019.
+//  Copyright © 2019 kazaimazai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for AVCompressor.
+FOUNDATION_EXPORT double AVCompressorVersionNumber;
+
+//! Project version string for AVCompressor.
+FOUNDATION_EXPORT const unsigned char AVCompressorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <AVCompressor/PublicHeader.h>
+
+
