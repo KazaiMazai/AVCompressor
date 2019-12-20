8bc7efb143d6b4d545d46b68ffbd7d81b26c04c9
Sergey Kazakov <kazaimazai@gmail.com>
2019-12-20 15:49:57 +0300

    implemented base compressor wrapper

---
+++ AVCompressor/Compressor.swift
@@ -0,0 +1,28 @@
+//
+//  Compressor.swift
+//  AVCompressor
+//
+//  Created by Sergey Kazakov on 29/11/2019.
+//  Copyright © 2019 kazaimazai. All rights reserved.
+//
+
+import Foundation
+
+public final class Compressor<Base> {
+  public let base: Base
+  public init(_ base: Base) {
+    self.base = base
+  }
+}
+
+public protocol CompressorСompatible {
+  associatedtype CompatibleType
+  var compressor: CompatibleType { get }
+}
+
+public extension CompressorСompatible {
+  var compressor: Compressor<Self> {
+    return Compressor(self)
+  }
+}
+
