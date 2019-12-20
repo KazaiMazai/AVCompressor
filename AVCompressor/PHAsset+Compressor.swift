7d8f09df5698dde46cfece24caa8654de8602a1e
Sergey Kazakov <kazaimazai@gmail.com>
2019-12-20 17:25:01 +0300

    added PHAsset compressor extension

---
+++ AVCompressor/PHAsset+Compressor.swift
@@ -0,0 +1,14 @@
+//
+//  PHAsset+AVCompressor.swift
+//  AVCompressor
+//
+//  Created by Sergey Kazakov on 20/12/2019.
+//  Copyright © 2019 kazaimazai. All rights reserved.
+//
+
+import Foundation
+import Photos
+
+extension Compressor where Base: PHAsset {
+  
+}
